a61c3f458881276e1bfced37f7c92f620ea20c07
OliGee <ngarcia333@gmail.com>
2024-05-25 10:27:32 +0200

    Implemented auth-callback

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency: 'EUR'
+  });
+
+  return formatter.format(price / 100);
+};
