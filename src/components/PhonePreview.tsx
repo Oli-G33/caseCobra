d0073c7f05a94d4de23e7e62488c1babc9d69986
OliGee <ngarcia333@gmail.com>
2024-05-25 13:21:24 +0200

    Initiated Thank-you page

---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { CaseColor } from '@prisma/client';
+
+const PhonePreview = ({
+  croppedImageUrl,
+  color
+}: {
+  croppedImageUrl: string;
+  color: CaseColor;
+}) => {};
+
+export default PhonePreview;
